569853fe6067ea90c47253486e06bef30414263b
Kai Pastor <dg0yt@darc.de>
2022-04-08 19:37:51 +0200

    WIP

--- ports/wxwidgets/vcpkg-cmake-wrapper.cmake
+++ ports/wxwidgets/vcpkg-cmake-wrapper.cmake
@@ -2,13 +2,20 @@ set(wxWidgets_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../.." CACHE INTERNAL "" FORCE
 set(WX_ROOT_DIR "${wxWidgets_ROOT_DIR}")
 set(wxWidgets_LIB_DIR "${wxWidgets_ROOT_DIR}/lib" CACHE INTERNAL "" FORCE)
 set(WX_LIB_DIR "${wxWidgets_LIB_DIR}")
+file(GLOB _vcpkg_wx_configs LIST_DIRECTORIES false "${wxWidgets_LIB_DIR}/wx/config/*")
+if(_vcpkg_wx_configs)
+    list(GET _vcpkg_wx_configs 0 wxWidgets_CONFIG_EXECUTABLE)
+endif()
+#[[
 find_library(WX_based NAMES wxbase31ud PATHS "${wxWidgets_ROOT_DIR}/debug/lib" NO_DEFAULT_PATH)
 file(GLOB WX_DEBUG_LIBS "${wxWidgets_ROOT_DIR}/debug/lib/wx*d_*.lib")
 foreach(WX_DEBUG_LIB ${WX_DEBUG_LIBS})
     string(REGEX REPLACE ".*wx([^/]*)d_([^/\\.]*)\\.[^/\\.]*\$" "WX_\\2d" varname "${WX_DEBUG_LIB}")
     set(${varname} "${WX_DEBUG_LIB}" CACHE INTERNAL "" FORCE)
 endforeach()
+]]
 _find_package(${ARGS})
+#[[
 find_package(ZLIB QUIET)
 find_package(libpng CONFIG QUIET)
 find_package(TIFF QUIET)
@@ -23,3 +30,4 @@ if(wxWidgets_LIBRARIES AND NOT wxWidgets_LIBRARIES MATCHES "TIFF::TIFF;png;expat
         list(APPEND wxWidgets_LIBRARIES "png_static")
     endif()
 endif()
+]]
