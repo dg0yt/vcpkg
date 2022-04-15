5b1b1f81ad1ce51ae159f9be5ebf78f3aeae9bd2
Kai Pastor <dg0yt@darc.de>
2022-04-15 12:28:10 +0200

    Reduce dependencies on qt5-base[gui]

--- ports/qt5-declarative/portfile.cmake
+++ ports/qt5-declarative/portfile.cmake
@@ -1,5 +1,16 @@
+vcpkg_list(SET OPTIONS)
+if(NOT "quick" IN_LIST FEATURES)
+    vcpkg_list(APPEND OPTIONS "QT.gui.name=" "QT.widgets.name=")
+endif()
+
 include(${CURRENT_INSTALLED_DIR}/share/qt5/qt_port_functions.cmake)
-qt_submodule_installation(OUT_SOURCE_PATH SOURCE_PATH PATCHES limits_include.patch)
+qt_submodule_installation(
+    OUT_SOURCE_PATH SOURCE_PATH
+    PATCHES
+        limits_include.patch
+    OPTIONS
+        ${OPTIONS}
+)
 
 if(NOT QT_UPDATE_VERSION)
   vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/qt5/QtQml/5.15.3/QtQml/private/qqmljsparser_p.h" "${SOURCE_PATH}" "")
--- ports/qt5-tools/portfile.cmake
+++ ports/qt5-tools/portfile.cmake
@@ -1,8 +1,33 @@
+vcpkg_list(SET libs quick widgets)
+vcpkg_list(SET OPTIONS)
+if(NOT "quick" IN_LIST FEATURES)
+    vcpkg_list(REMOVE_ITEM libs quick)
+    vcpkg_list(APPEND OPTIONS "QT.quick.name=")
+endif()
+if(NOT "widgets" IN_LIST FEATURES)
+    vcpkg_list(REMOVE_ITEM libs widgets)
+    vcpkg_list(APPEND OPTIONS
+        "config.qttools.features.assistant.disable=true"
+        "config.qttools.features.designer.disable=true"
+        "config.qttools.features.distancefieldgenerator.disable=true"
+        "config.qttools.features.pixeltool.disable=true"
+        "config.qttools.features.qtdiag.disable=true"
+    )
+endif()
+
 include(${CURRENT_INSTALLED_DIR}/share/qt5/qt_port_functions.cmake)
 
-qt_submodule_installation(PATCHES
-    icudt-debug-suffix.patch # https://bugreports.qt.io/browse/QTBUG-87677
+qt_submodule_installation(
+    PATCHES
+        icudt-debug-suffix.patch # https://bugreports.qt.io/browse/QTBUG-87677
+    OPTIONS
+        ${OPTIONS}
 )
+
+if(NOT libs)
+    set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+    file(REMOVE "${CURRENT_PACKAGES_DIR}/debug")
+endif()
 
 if(EXISTS "${CURRENT_INSTALLED_DIR}/plugins/platforms/qminimal${VCPKG_TARGET_SHARED_LIBRARY_SUFFIX}")
     file(INSTALL "${CURRENT_INSTALLED_DIR}/plugins/platforms/qminimal${VCPKG_TARGET_SHARED_LIBRARY_SUFFIX}" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin/plugins/platforms")
