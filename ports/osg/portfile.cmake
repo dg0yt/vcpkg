879797e5d1677744b6c65ba189472d201cbaa44b
Kai Pastor <dg0yt@darc.de>
2022-05-15 12:11:17 +0200

    Merge curl patches

--- ports/osg/portfile.cmake
+++ ports/osg/portfile.cmake
@@ -8,7 +8,6 @@ vcpkg_from_github(
 	HEAD_REF master
     PATCHES
         collada.patch
-        static.patch
         fix-sdl.patch
         fix-example-application.patch
         disable-present3d-staticview-in-linux.patch #Due to some link error we cannot solve yet, disable them in linux.
