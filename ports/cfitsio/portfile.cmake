a6b26930ed1581794ee407fd2072f2df78abad30
Kai Pastor <dg0yt@darc.de>
2022-09-15 19:27:45 +0200

    WIP

--- ports/cfitsio/portfile.cmake
+++ ports/cfitsio/portfile.cmake
@@ -6,66 +6,46 @@ vcpkg_download_distfile(ARCHIVE
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
+    ARCHIVE "${ARCHIVE}"
     PATCHES
         0001-fix-dependencies-and-export-cmake-targets.patch
         0002-add-Wno-error-implicit-funciton-declaration-to-cmake.patch
         0003-fix-LNK2019-of-strtok_r-in-pthreads-feature.patch
-        0004-pkg-config.patch
 )
 
 vcpkg_check_features(
     OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         curl        USE_CURL
+        curl        CMAKE_REQUIRE_FIND_PACKAGE_CURL
+        pthreads    USE_PTHREADS
+        tools       UTILS
 )
-
-set(PKG_CONFIG_REQUIRES_PRIVATE zlib)
-if ("curl" IN_LIST FEATURES)
-    set(FIND_CURL_DEPENDENCY "find_dependency(CURL CONFIG)")
-    string(APPEND PKG_CONFIG_REQUIRES_PRIVATE " libcurl")
-endif()
-
-if ("pthreads" IN_LIST FEATURES)
-    if (VCPKG_TARGET_IS_WINDOWS)
-        set(WITH_PTHREADS ON)
-        set(FIND_PTHREADS_DEPENDENCY "find_dependency(pthreads)")
-    else()
-        message(WARNING "Feature pthreads only support Windows, disable it now.")
-        set(WITH_PTHREADS OFF)
-    endif()
-endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    DISABLE_PARALLEL_CONFIGURE
     OPTIONS
         ${FEATURE_OPTIONS}
-        -DUSE_PTHREADS=${WITH_PTHREADS}
-        "-DPKG_CONFIG_REQUIRES_PRIVATE=${PKG_CONFIG_REQUIRES_PRIVATE}"
-        -DPKG_CONFIG_LIBS=-lcfitsio
+    OPTIONS_DEBUG
+        -DUTILS=OFF
+    MAYBE_UNUSED_VARIABLES
+        CMAKE_REQUIRE_FIND_PACKAGE_CURL
 )
 
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
 vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-cfitsio)
 
-file(READ "${CURRENT_PACKAGES_DIR}/share/unofficial-cfitsio/unofficial-cfitsio-config.cmake" ASSIMP_CONFIG)
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/unofficial-cfitsio/unofficial-cfitsio-config.cmake" "
-include(CMakeFindDependencyMacro)
-${FIND_CURL_DEPENDENCY}
-${FIND_PTHREADS_DEPENDENCY}
-find_dependency(ZLIB)
-${ASSIMP_CONFIG}
-")
+if("tools" IN_LIST FEATURES)
+    vcpkg_copy_tools(
+        TOOL_NAMES FPack Funpack Fitscopy
+        AUTO_CLEAN
+    )
+endif()
 
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/unofficial-cfitsio/unofficial-cfitsio-config.cmake"
-    "cmake_policy(VERSION 2.6)"
-    "cmake_policy(VERSION 2.6)\r\n\
-# Required for the evaluation of \"if(@BUILD_SHARED_LIBS@)\" below to function\r\n\
-cmake_policy(SET CMP0012 NEW)\r\n"
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
 )
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/include/unistd.h" "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(INSTALL "${SOURCE_PATH}/License.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
