d34eabe583d16561863ef5c0a36ea05e864895a6
Kai Pastor <dg0yt@darc.de>
2023-04-20 07:30:52 +0200

    [vulkan] Improve error and usage reporting

--- ports/vulkan/portfile.cmake
+++ ports/vulkan/portfile.cmake
@@ -1,24 +1,40 @@
-# Due to the complexity involved, this package just verifies the Vulkan SDK is installed.
-
-# Stub only port
+# This package just verifies that the Vulkan SDK is installed.
 set(VCPKG_BUILD_TYPE release)
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-# The VULKAN_SDK environment variable is only a robust check on WIN32.
-if (WIN32 AND NOT DEFINED ENV{VULKAN_SDK})
-    message(FATAL_ERROR "VULKAN_SDK environment variable not set! Refer to the Windows getting started documentation: https://vulkan.lunarg.com/doc/sdk/latest/windows/getting_started.html")
+if(DEFINED ENV{VULKAN_SDK})
+    message(STATUS "VULKAN_SDK environment variable: $ENV{VULKAN_SDK}")
+else()
+    message(STATUS "VULKAN_SDK environment variable: not set")
 endif()
 
+set(vulkan_result_file "${CURRENT_BUILDTREES_DIR}/vulkan-${TARGET_TRIPLET}.cmake.log")
 vcpkg_cmake_configure(
     SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}"
+    OPTIONS
+        "-DVCPKG_VULKAN_VERSION=${VERSION}"
+    OPTIONS_RELEASE
+        "-DOUTFILE=${vulkan_result_file}"
 )
 
-if (EXISTS ${VULKAN_DIR}/../LICENSE.txt)
-    configure_file(${VULKAN_DIR}/../LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/vulkan/copyright COPYONLY)
-elseif(EXISTS ${VULKAN_DIR}/LICENSE.txt)
-    configure_file(${VULKAN_DIR}/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/vulkan/copyright COPYONLY)
+include("${vulkan_result_file}")
+if(DETECTED_Vulkan_FOUND)
+    message(STATUS "Found Vulkan SDK ${DETECTED_Vulkan_VERSION} (${DETECTED_Vulkan_LIBRARIES})")
 else()
-    configure_file(${CURRENT_PORT_DIR}/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/vulkan/copyright COPYONLY)
+    set(message "The Vulkan SDK wasn't found. ")
+    if(VCPKG_TARGET_IS_WINDOWS)
+        string(APPEND message "Refer to Getting Started with the Windows Vulkan SDK: https://vulkan.lunarg.com/doc/sdk/latest/windows/getting_started.html")
+    elseif(VCPKG_TARGET_IS_OSX)
+        string(APPEND message "Refer to Getting Started with the macOS Vulkan SDK: https://vulkan.lunarg.com/doc/sdk/latest/mac/getting_started.html")
+    elseif(VCPKG_TARGET_IS_LINUX)
+        string(APPEND message "Refer to Getting Started with the Linux Tarball Vulkan SDK: https://vulkan.lunarg.com/doc/sdk/latest/linux/getting_started.html")
+    else()
+        string(APPEND message "Please set environment variable VULKAN_SDK.")
+    endif()
+    message(FATAL_ERROR "${message}")
 endif()
+
+find_file(vulkan_license NAMES LICENSE.txt PATHS ${DETECTED_Vulkan_INCLUDE_DIRS} "${CURRENT_PORT_DIR}" PATH_SUFFIXES "..")
+vcpkg_install_copyright(FILE_LIST "${vulkan_license}")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
