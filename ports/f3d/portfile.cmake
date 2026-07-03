ce2647c4809edf2427a03812d72909ecb8a8bf3c
Kai Pastor <dg0yt@darc.de>
2026-07-03 06:40:37 +0200

    WIP

--- ports/f3d/portfile.cmake
+++ ports/f3d/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO f3d-app/f3d
     REF v${VERSION}
-    SHA512 210845e20af37288a069a0a00f35c989d662de27027e6d841cec2eea43c1c5beb66dcf68555bfc22b656e086522de7c5fe52431f2875e70939f15d9aa05a015c
+    SHA512 da302baff8294af87032d238552f46dbd6ebce04dce2a8ec4711c79398f01d38641b2a8e7da4ab3261c555c6eaf84b73c91e3b100a3a85ef1e1d24b3a654d79e
     HEAD_REF master
     PATCHES
         fix-install.patch
@@ -25,25 +25,22 @@ vcpkg_cmake_configure(
         ${FEATURE_OPTIONS}
         -DF3D_MACOS_BUNDLE=OFF
         -DF3D_WINDOWS_BUILD_SHELL_THUMBNAILS_EXTENSION=OFF
+        -DVCPKG_TRACE_FIND_PACKAGE=1
     MAYBE_UNUSED_VARIABLES
         F3D_MACOS_BUNDLE
         F3D_WINDOWS_BUILD_SHELL_THUMBNAILS_EXTENSION
 )
-
 vcpkg_cmake_install()
-
+vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/f3d)
 
-# If the application feature is enabled, install it as a tool
 if("application" IN_LIST FEATURES)
     vcpkg_copy_tools(TOOL_NAMES f3d AUTO_CLEAN)
 endif()
 
-vcpkg_copy_pdbs()
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+#file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
